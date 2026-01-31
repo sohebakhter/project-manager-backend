4c7a8eaf9e62fe09fa820098aa3fd4e8efaf736d
sohebakhter <sohebakhterbadhan@gmailcom>
2026-01-31 19:32:52 +0600

    fixed cors in index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/auth', authRoutes);
